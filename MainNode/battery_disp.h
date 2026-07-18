cd9f76e8a813f021ae5a685b3a111303785b6922
pavaniviswanadhula <127957858+pavaniviswanadhula@users.noreply.github.com>
2026-07-18 11:19:22 +0530

    Delete MainNode directory

--- MainNode/battery_disp.h
+++
@@ -1,9 +0,0 @@
-#ifndef _BATTERY_DISP_H_
-#define _BATTERY_DISP_H_
-void StoreCustCharFont(unsigned char p);
-void StoreCustCharFont1(unsigned char p);
-void StoreCustCharFont2(unsigned char p);
-void StoreCustCharFont3(unsigned char p);
-void StoreCustCharFont4(unsigned char p);
-#endif
-
--- MainNode/lcd.h
+++
@@ -1,7 +0,0 @@
-void writelcd(char ch);
-void cmdlcd(char cmd);
-void initlcd(void);
-void charlcd(char dat);
-void strlcd(char *p);
-void u32lcd(signed int n);
-void f32lcd(float f,char i);
